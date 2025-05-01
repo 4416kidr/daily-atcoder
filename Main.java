ba7e57659bebc558abd37f44357cb34cb24984de
TOFU <4416kidr@gmail.com>
2025-05-02 01:27:02 +0900

    AC: 395B

--- Main.java
+++ Main.java
@@ -3,26 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int inptQ = sc.nextInt();
+        int inptN = sc.nextInt();
         sc.nextLine();
         sc.close();
 
-        int[] deck = new int[100];
-        int pointer = 0;
-        for (int i = 0; i < inptQ; i++) {
-            int query = sc.nextInt();
-            if (query == 1) {
-                deck[pointer] = sc.nextInt();
-                pointer += 1;
-            } else {
-                if (pointer <= 0) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(deck[pointer-1]);
-                    deck[pointer-1] = -1;
-                    pointer -= 1;
-                }
+        // i = 1, 2, ..., N
+        // j = N+1-i
+        // i <= j ---> i % 2 == 1 ? "#" : "."
+
+        char[][] area = new char[inptN][inptN];
+        for (int i = 0; i < inptN; i++) {
+            int j = inptN - i; //j = N + 1 - i
+            char color = i % 2 == 0 ? '#' : '.';
+            square(i, j, color, area, inptN);
+        }
+
+        // System.out.println("------------------");
+        for (int i = 0; i < inptN; i++) {
+            System.out.println(new String(area[i]));
+        }
+    }
+    private static char[][] square(int inptI, int inptJ, char color, char[][] area, int inptN) {
+        for (int i = inptI; i < inptJ; i++) {
+            for (int j = inptI; j < inptJ; j++) {
+                area[i][j] = color;
             }
         }
+
+        // for (int i = 0; i < inptN; i++) {
+        //     System.out.println(new String(area[i]));
+        // }
+        return area;
     }
 }
