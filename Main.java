8ac70037b922c2e3ee8d906674ad62d2c64c1017
TOFU <4416kidr@gmail.com>
2025-05-05 00:24:20 +0900

    AC: 100C

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,19 +11,14 @@ public class Main {
         }
         sc.close();
 
-        long ansP = 0;
-        for (int i = 0; i < inptN; i++) {
-            // 要素1の配列は条件を満たす
-            ansP++;
-            // 要素2以上の配列で探索
-            for (int j = i+1; j < inptN; j++) {
-                if (seriesA[j-1] >= seriesA[j]) {
-                    // 単調増加でなくなった場合、それ以降は単調増加配列という条件を満たさなくなるので探索を終了する
-                    break;
-                }
-                ansP++;
+        int evenCount = 0;
+        for (int a : seriesA) {
+            int nxt = a;
+            while (nxt % 2 == 0) {
+                evenCount += 1;
+                nxt /= 2;
             }
         }
-        System.out.println(ansP);
+        System.out.println(evenCount);
     }
 }
