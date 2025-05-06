9b2cf568fb566ba3d2bccd9a3620d3a77b641dc4
TOFU <4416kidr@gmail.com>
2025-05-07 02:45:21 +0900

    AC: 150C for版

--- Main.java
+++ Main.java
@@ -16,7 +16,33 @@ public class Main {
         }
         sc.close();
 
-        System.out.println(abs(func(seriesP, 1), func(seriesQ, 1)));
+        int ra = 1;
+        int rb = 1;
+        for (int i = 0; i < inptN; i++) {
+            int[] sortedArrayP = Arrays.copyOfRange(seriesP, i, inptN);
+            Arrays.sort(sortedArrayP);
+            int[] sortedArrayQ = Arrays.copyOfRange(seriesQ, i, inptN);
+            Arrays.sort(sortedArrayQ);
+
+            boolean[] flags = new boolean[]{false, false};
+            int[] inds = new int[]{-1, -1};
+            for (int j = 0; j < sortedArrayP.length; j++) {
+                if (seriesP[i] == sortedArrayP[j]) {
+                    inds[0] = j;
+                    flags[0] = true;
+                }
+                if (seriesQ[i] == sortedArrayQ[j]) {
+                    inds[1] = j;
+                    flags[1] = true;
+                }
+                if (flags[0] && flags[1]) {
+                    break;
+                }
+            }
+            ra += inds[0] * permutation(inptN - 1 - i);
+            rb += inds[1] * permutation(inptN - 1 - i);
+        }
+        System.out.println(Math.abs(ra-rb));
     }
     private static int permutation(int n) {
         int res = 1;
@@ -24,28 +50,5 @@ public class Main {
             res *= i;
         }
         return res;
-    }
-    private static int func(int[] series, int v) {
-        if (series.length == 1) {
-            return v;
-        }
-        // インデックス検索の準備
-        int[] sortedArray = Arrays.copyOf(series, series.length);
-        Arrays.sort(sortedArray);
-        
-        // index of
-        final int a = series[0];
-        int ind = -1;
-        for (int i = 0; i < series.length; i++) {
-            if (a == sortedArray[i]) {
-                ind = i;
-                break;
-            }
-        }
-        return func(Arrays.copyOfRange(series, 1, series.length), v + ind * permutation(series.length-1));
-    }
-    private static int abs(int a, int b) {
-        // Math.abs(a-b);
-        return a - b < 0 ? -(a-b) : a-b;
     }
 }
