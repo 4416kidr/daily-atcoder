a32d47d9376ecb1b981e7df333dccf5b2545e2cc
TOFU <4416kidr@gmail.com>
2025-05-09 01:38:10 +0900

    add: 392B with debug

--- Main.java
+++ Main.java
@@ -4,26 +4,55 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String series = sc.nextLine();
+        int inptN = sc.nextInt();
+        int inptM = sc.nextInt();
+        int[] seriesA = new int[inptM];
+        for (int i = 0; i < inptM; i++) {
+            seriesA[i] = sc.nextInt();
+        }
         sc.close();
+        
+        Arrays.sort(seriesA);
+        System.out.println(inptN + ", " + inptM);
+        System.out.println(Arrays.toString(seriesA));
 
-        int count = 0;
-        for (int i = 0; i < series.length(); i++) {
-            if (series.charAt(i) != 'A') {
+        int ap = 0;
+        int bp = 0;
+        boolean isHit = false;
+        int[] seriesAns = new int[inptN];
+        for (int i = 1; i <= inptN; i++) {
+            // out of index
+            if (ap >= inptM) {
+                System.out.println("ap >= inptM");
+                seriesAns[bp] = i;
+                bp++;
                 continue;
             }
-            for (int j = i+1; j < series.length(); j++) {
-                if (series.charAt(j) != 'B') {
-                    continue;
-                }
-                if (j + (j-i) >= series.length()) {
-                    continue;
-                }
-                if (series.charAt(j + (j-i)) == 'C') {
-                    count += 1;
+            isHit = false;
+            for (int j = ap; j < inptM; j++) {
+                System.out.println("[" + i + ", " + j + "]: " + seriesA[j] + ", " + isHit);
+                if (seriesA[j] == i) {
+                    isHit = true;
+                    ap += 1;
+                    break;
                 }
             }
+            System.out.println("isHit: " + isHit);
+            if (!isHit) {
+                System.out.println("add");
+                seriesAns[bp] = i;
+                bp++;
+            }
         }
-        System.out.println(count);
+        System.out.println("length: " + seriesAns.length + ", " + bp);
+        String ans = "";
+        for (int i = 0; i < bp; i++) {
+            if (i == 0) {
+                ans += seriesAns[i];
+                continue;
+            }
+            ans += " " + seriesAns[i];
+        }
+        System.out.println(bp + "\n" + ans);
     }
 }
