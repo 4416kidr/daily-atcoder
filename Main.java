305a7e1fb144bbc41819bc8cefdac53ce9427d34
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-10 03:37:44 +0900

    add: 200C.v4 with debug

--- Main.java
+++ Main.java
@@ -13,13 +13,25 @@ public class Main {
         sc.close();
         
         Arrays.sort(seriesA);
+        System.out.println(Arrays.toString(seriesA));
+        int startPointer = 0;
+        int endPointer = 1;
         int count = 0;
-        for (int i = 0; i < inptN; i++) {
-            for (int j = i+1; j < inptN; j++) {
-                if (seriesA[i] != seriesA[j]) {
-                    break;
+        int loopCount = 0;
+        while(endPointer < inptN) {
+            System.out.print(String.format("[%1$d] [%2$d, %3$d]: %4$d, %5$d", ++loopCount, startPointer, endPointer, seriesA[startPointer], seriesA[endPointer]));
+            if (seriesA[startPointer] == seriesA[endPointer]) {
+                // 区切りの探索
+                System.out.println();
+                endPointer++;
+            } else {
+                // countの更新: combination(同じ値の個数, 2)
+                for (int i = 1; i < endPointer - startPointer; i++) {
+                    count += i;
                 }
-                count++;
+                // Pointerの更新と次の探索へ
+                System.out.println(String.format("\n--------- updated(%1$d) ---------", count));
+                startPointer = endPointer++;
             }
         }
         System.out.println(count);
