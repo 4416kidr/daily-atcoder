75d7385a33ad6862814f93a529c20712b054daf7
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-10 04:16:30 +0900

    add: 200C.v5d forの前後に注意を払って修正

--- Main.java
+++ Main.java
@@ -15,16 +15,33 @@ public class Main {
         Arrays.sort(seriesA);
         int startPointer = 0;
         int count = 0;
+        int tempSameCount = 1;
+        System.out.println(Arrays.toString(seriesA));
         for (int endPointer = 1; endPointer < inptN; endPointer++) {
+            System.out.println(String.format("  [%1$d] [%2$d, %3$d]: %4$d, %5$d (%6$d)", endPointer, startPointer, endPointer, seriesA[startPointer], seriesA[endPointer], tempSameCount));
             if (seriesA[startPointer] == seriesA[endPointer]) {
-                // まだ区切りではない
+                tempSameCount++;
+                continue;
+            }
+            // Pointerの更新と次の探索へ
+            startPointer = endPointer;
+            if (tempSameCount <= 1) {
+                System.out.println("--------- same count <= 1 ---------");
                 continue;
             }
             // countの更新: combination(同じ値の個数, 2)
-            count += (endPointer - startPointer) * (endPointer - startPointer - 1) / 2;
-            // Pointerの更新と次の探索へ
-            startPointer = endPointer;
+            count += tempSameCount * (tempSameCount - 1) / 2;
+            System.out.println(String.format("--------- sameCount(%1$d) updated(%2$d) ---------", tempSameCount, count));
+            tempSameCount = 1;
         }
+        System.out.println(String.format("\n--- final(%1$d) ---", tempSameCount));
+
+        if (tempSameCount > 1) {
+            count += tempSameCount * (tempSameCount - 1) / 2;
+            System.out.println(String.format("  updated(%1$d)", count));
+        }
+
+
         System.out.println(count);
     }
 }
