c6798fdfcd0c4d8beb5d6aa0bf6ca8aa5c61dc38
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-10 03:52:31 +0900

    add: 200C.v4.2d for文に変更

--- Main.java
+++ Main.java
@@ -14,21 +14,19 @@ public class Main {
         
         Arrays.sort(seriesA);
         int startPointer = 0;
-        int endPointer = 1;
         int count = 0;
         int loopCount = 0;
-        while(endPointer < inptN) {
+        for (int endPointer = 1; endPointer < inptN; endPointer++) {
             System.out.println(String.format("  [%1$d] [%2$d, %3$d]: %4$d, %5$d", ++loopCount, startPointer, endPointer, seriesA[startPointer], seriesA[endPointer]));
-            if (seriesA[startPointer] != seriesA[endPointer]) {
-                // 区切りに到達
-                // countの更新: combination(同じ値の個数, 2)
-                count += (endPointer - startPointer) * (endPointer - startPointer - 1) / 2;
-                System.out.println(String.format("--------- sameCount(%1$d) updated(%2$d) ---------", endPointer - startPointer, count));
-                // Pointerの更新と次の探索へ
-                startPointer = endPointer;
+            if (seriesA[startPointer] == seriesA[endPointer]) {
+                // まだ区切りではない
+                continue;
             }
-            // 区切り探索時はもちろん、区切りに到達後も更新が必要
-            endPointer++;
+            // countの更新: combination(同じ値の個数, 2)
+            count += (endPointer - startPointer) * (endPointer - startPointer - 1) / 2;
+            System.out.println(String.format("--------- sameCount(%1$d) updated(%2$d) ---------", endPointer - startPointer, count));
+            // Pointerの更新と次の探索へ
+            startPointer = endPointer;
         }
         System.out.println(count);
     }
