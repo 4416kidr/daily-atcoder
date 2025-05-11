a22d04ec16c0fce1821355189f79108ec8c026cc
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-12 01:34:22 +0900

    AC: 400A

--- Main.java
+++ Main.java
@@ -1,43 +1,11 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int inptN = sc.nextInt();
-        int[] seriesA = new int[inptN];
-        for (int i = 0; i < inptN; i++) {
-            // 基数200の世界を扱う
-            seriesA[i] = sc.nextInt() % 200;
-        }
+        short inptA = sc.nextShort();
         sc.close();
-        
-        Arrays.sort(seriesA);
-        int startPointer = 0;
-        long count = 0;
-        int tempSameCount = 1;
-        for (int endPointer = 1; endPointer < inptN; endPointer++) {
-            if (seriesA[startPointer] == seriesA[endPointer]) {
-                tempSameCount++;
-                continue;
-            }
-            // Pointerの更新と次の探索へ
-            startPointer = endPointer;
-            if (tempSameCount <= 1) {
-                continue;
-            }
-            // countの更新: combination(同じ値の個数, 2)
-            count += combinationPreventOverFlow(tempSameCount);
-            tempSameCount = 1;
-        }
-
-        count += tempSameCount > 1 ? combinationPreventOverFlow(tempSameCount) : 0;
-        System.out.println(count);
-    }
-
-    public static long combinationPreventOverFlow(int n) {
-        int a = n % 2 == 0 ? n / 2 : n;
-        int b = (n-1) % 2 == 0 ? (n-1) / 2 : n-1;
-        return (long)a*(long)b;
+        final short PEOPLE = 400;
+        System.out.println(PEOPLE % inptA == 0 ? (short)(PEOPLE / inptA) : -1);
     }
 }
