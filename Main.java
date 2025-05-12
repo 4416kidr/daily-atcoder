1bb72f7e0af9247df6de377d126af099ac98bccf
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-13 01:18:54 +0900

    add: 350B.d1 入力例クリア

--- Main.java
+++ Main.java
@@ -1,11 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        short inptA = sc.nextShort();
+        int inptN = sc.nextInt();
+        int inptQ = sc.nextInt();
+        sc.nextLine();
+        int[] seriesT = new int[inptQ];
+        for (int i = 0; i < inptQ; i++) {
+            seriesT[i] = sc.nextInt();
+        }
         sc.close();
-        final short PEOPLE = 400;
-        System.out.println(PEOPLE % inptA == 0 ? (short)(PEOPLE / inptA) : -1);
+        System.out.println(inptN + ", " + inptQ);
+        System.out.println(Arrays.toString(seriesT));
+        
+        boolean[] noTeeth = new boolean[inptN];
+        System.out.println(Arrays.toString(noTeeth));
+        for (int s : seriesT) {
+            noTeeth[s-1] = !noTeeth[s-1];
+        }
+        System.out.println(Arrays.toString(noTeeth));
+        int count = 0;
+        for (boolean b : noTeeth) {
+            count += b ? 0 : 1;
+        }
+        System.out.println(count);
+
     }
 }
