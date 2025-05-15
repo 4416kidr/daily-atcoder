3e4a994286993ec57297272702d4b96ec8fe01a9
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-15 20:40:33 +0900

    add: 391B Stringにした

--- Main.java
+++ Main.java
@@ -8,16 +8,30 @@ public class Main {
         final short inptM = sc.nextShort();
         sc.nextLine();
         System.out.println(inptN + ", " + inptM);
-        final char[][] tableS = inputTable(inptN, sc);
-        final char[][] tableT = inputTable(inptM, sc);
+        final String[] tableS = inputTable(inptN, sc);
+        final String[] tableT = inputTable(inptM, sc);
         sc.close();
-    }
-    private static char[][] inputTable(short n, Scanner sc) {
-        char[][] array = new char[n][n];
-        for (short i = 0; i < n; i++) {
-            array[i] = String.join("", sc.nextLine().split(" ")).toCharArray();
+
+        // 縦検査
+        for (short i = 0; i < inptN - inptM + 1; i++) {
+            if (!tableS[i].contains(tableT[0]))
+                continue;
+            System.out.println("line" + i);
+            // 列を調べる
+            // 各行の含有チェック
+            for (short j = 1; j < inptM; j++) {
+                if (tableS[i+j].contains(tableS[j]))
+                    System.out.println(String.format("%1$d %2$d", i, j));
+                
+            }
         }
-        Arrays.asList(array).forEach(l -> System.out.println(Arrays.toString(l)));
+    }
+    private static String[] inputTable(short n, Scanner sc) {
+        String[] array = new String[n];
+        for (short i = 0; i < n; i++) {
+            array[i] = sc.nextLine();
+        }
+        System.out.println(Arrays.toString(array));
         return array;
     }
 }
