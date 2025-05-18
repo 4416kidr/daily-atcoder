66dc20365be9980f91bf5d8f365247a01e9668e9
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-18 19:59:57 +0900

    add: 387B pass sample

--- Main.java
+++ Main.java
@@ -1,61 +1,24 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Main {
-    // 16m45s
-    // ABC388B
+    // 22m26s
+    // ABC387B
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final int N = sc.nextInt();
-        final int D = sc.nextInt();
-        List<Snake> snakes = new ArrayList<>();
-        IntStream.range(0, N).forEach(i -> snakes.add(new Snake(sc.nextInt(), sc.nextInt())));
+        final int X = sc.nextInt();
         sc.close();
-        solve(D, snakes);
+        solve(X);
     }
-    private static void solve(int D, List<Snake> snakes) {
-        IntStream.range(1, D+1).forEach(i -> {
-            snakes.forEach(s -> s.incrementLength());
-            System.out.println(Collections.max(snakes).getWeight());
-        });
-    }
-}
-
-class Snake extends Object implements Comparable<Snake> {
-    private final int thick;
-    private int length;
-
-    public Snake(int thick, int length) {
-        this.thick = thick;
-        this.length = length;
-    }
-
-    public int getThick() {
-        return this.thick;
-    }
-
-    public int getLength() {
-        return this.length;
-    }
-
-    public void incrementLength() {
-        this.length++;
-    }
-
-    public int getWeight() {
-        return this.thick * this.length;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("thick(%1$d) * length(%2$d) = weight(%3$d)", this.thick, this.length, this.getWeight());
-    }
-
-    @Override
-    public int compareTo(Snake sn) {
-        return Integer.compare(this.getWeight(), sn.getWeight());
+    private static void solve(int inptX) {
+        int sum = 0;
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                if (inptX == i * j) {
+                    continue;
+                }
+                sum += i * j;
+            }
+        }
+        System.out.println(sum);
     }
 }
