1be716b581c3cb64f3fa1b0dac44ebab81566366
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-19 20:57:01 +0900

    AC: 390A

--- Main.java
+++ Main.java
@@ -1,24 +1,31 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
-    // 22m26s
-    // ABC387B
+    // 24m55s
+    // ABC390A
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final int X = sc.nextInt();
+        List<Integer> list = IntStream.range(0, 5).map(i -> sc.nextInt()).boxed().toList();
         sc.close();
-        solve(X);
+        boolean ans = solve(list);
+        System.out.println(ans ? "Yes" : "No");
     }
-    private static void solve(int inptX) {
-        int sum = 0;
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                if (inptX == i * j) {
-                    continue;
-                }
-                sum += i * j;
-            }
+
+    private static boolean solve(List<Integer> list) {
+        // 実際にスワップしてみる
+        if (isSorted(list)) {
+            return false;
         }
-        System.out.println(sum);
+        return (int)IntStream.range(0, 4).filter(i -> isSorted(swap2At(list, i))).count() == 1;
+    }
+
+    private static boolean isSorted(List<Integer> list) {
+        return list.stream().allMatch(v -> v == (list.indexOf(v)+1));
+    }
+
+    private static List<Integer> swap2At(List<Integer> list, int ind) {
+        return IntStream.range(0, 5).map(i -> i == ind ? list.get(i+1) : i-1 == ind ? list.get(i-1) : list.get(i)).boxed().toList();
     }
 }
