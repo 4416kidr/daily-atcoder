15ff4a84a0c50da99158b7553df024b333989558
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-20 20:22:20 +0900

    AC: 386B

--- Main.java
+++ Main.java
@@ -1,32 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main {
-    // 24m55s
+    // ??m??s 多分20分ぐらい
     // ABC386B
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String inpt = sc.nextLine();
         sc.close();
-        System.out.println(inpt);
         System.out.println(solve(inpt));
     }
 
     private static int solve(String inpt) {
         Iterator<String> inpIterator = inpt.chars().mapToObj(c -> String.valueOf(c - '0')).collect(Collectors.toList()).iterator();
-        // List<String> inpIterator = inpt.chars().mapToObj(c -> String.valueOf(c - '0')).collect(Collectors.toList());
-        // inpIterator.forEachRemaining(actualList::add);
-        // System.out.println(inpIterator);
+
         int count = 0;
         boolean isZero = false;
         while (inpIterator.hasNext()) {
             char c = inpIterator.next().charAt(0);
-            System.out.println(String.format("[%1$s] %2$s, %3$s", c, count, isZero));
             if (c == '0') {
                 count += isZero ? 1 : 0;
                 isZero = !isZero;
@@ -43,10 +35,8 @@ public class Main {
             // 0出ないので加算
             count++;
         }
-        System.out.println("before finishize: " + count);
         // 終了処理 (0で終わった場合は1加算)
         count += isZero ? 1 : 0;
-        System.out.println("finish while: " + count);
         return count;
     }
 }
