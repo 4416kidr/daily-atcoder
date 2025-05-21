df1355fd56a265a31b663a583f5a32c4b84c1a83
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-22 08:28:54 +0900

    add: AC066C input

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
-    // 03m15s
-    // ABC387A
+    // ??m??s
+    // ABC066C
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final int A = sc.nextInt();
-        final int B = sc.nextInt();
+        final int N = sc.nextInt();
+        List<Integer> list = new ArrayList<>(N);
+        IntStream.range(0, N).forEach(i -> list.add(sc.nextInt()));
         sc.close();
-        System.out.println((A+B) * (A+B));
+        System.out.println(N + ", " + list);
     }
 }
