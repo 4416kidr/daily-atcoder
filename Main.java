4a3bc93784d29c431fe142195c354ce26f1e0f7a
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-21 21:57:53 +0900

    AC: 387A

--- Main.java
+++ Main.java
@@ -1,42 +1,13 @@
-import java.util.Iterator;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
-    // ??m??s 多分20分ぐらい
-    // ABC386B
+    // 03m15s
+    // ABC387A
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String inpt = sc.nextLine();
+        final int A = sc.nextInt();
+        final int B = sc.nextInt();
         sc.close();
-        System.out.println(solve(inpt));
-    }
-
-    private static int solve(String inpt) {
-        Iterator<String> inpIterator = inpt.chars().mapToObj(c -> String.valueOf(c - '0')).collect(Collectors.toList()).iterator();
-
-        int count = 0;
-        boolean isZero = false;
-        while (inpIterator.hasNext()) {
-            char c = inpIterator.next().charAt(0);
-            if (c == '0') {
-                count += isZero ? 1 : 0;
-                isZero = !isZero;
-                continue;
-            }
-            // 0が終わった
-            if (isZero) {
-                count++;
-                isZero = false;
-            }
-            if (c == 'z') {
-
-            }
-            // 0出ないので加算
-            count++;
-        }
-        // 終了処理 (0で終わった場合は1加算)
-        count += isZero ? 1 : 0;
-        return count;
+        System.out.println((A+B) * (A+B));
     }
 }
