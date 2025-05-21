d03068d21cc315d250b717cdc43094e65d3a34d1
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-22 08:37:53 +0900

    add: 050C Dataクラスを作成

--- Main.java
+++ Main.java
@@ -9,9 +9,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         final int N = sc.nextInt();
-        List<Integer> list = new ArrayList<>(N);
-        IntStream.range(0, N).forEach(i -> list.add(sc.nextInt()));
+        List<Data> list = new ArrayList<>(N);
+        IntStream.range(0, N).forEach(i -> list.add(new Data(sc.nextInt())));
         sc.close();
         System.out.println(N + ", " + list);
+        solve(list);
+    }
+
+    public static void solve(List<Data> list) {
+        System.out.println("solve");
+    }
+}
+
+class Data extends Object {
+    private int abs;
+
+    public Data(int abs) {
+        this.abs = abs;
+    }
+
+    public int getAbs() {
+        return this.abs;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%1$d", this.abs);
     }
 }
