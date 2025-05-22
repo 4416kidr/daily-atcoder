f2b07fdd4ff41ee80538fdc2359af533a2087425
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-22 19:55:43 +0900

    add: 050C 入力値が正常なのか判定

--- Main.java
+++ Main.java
@@ -18,10 +18,23 @@ public class Main {
 
     public static void solve(List<Data> list) {
         System.out.println("solve");
+        checkFormat(list);
+    }
+
+    public static boolean checkFormat(List<Data> list) {
+        list.sort((x, y) -> Integer.compare(x.getAbs(), y.getAbs()));
+        boolean isOK = false;
+        if (list.size() % 2 == 1) {
+            isOK = IntStream.range(0, list.size()).allMatch(i -> (i+1)/2 == list.get(i).getAbs());
+        } else {
+            isOK = IntStream.range(0, list.size()).allMatch(i -> i/2 == list.get(i).getAbs());
+        }
+        System.out.println(String.format("%1$s, isOK(%2$s), list: %3$s", list.size() % 2 == 0 ? "even" : "odd", isOK, list));
+        return isOK;
     }
 }
 
-class Data extends Object {
+class Data extends Object implements Comparable<Data> {
     private int abs;
 
     public Data(int abs) {
@@ -35,5 +48,10 @@ class Data extends Object {
     @Override
     public String toString() {
         return String.format("%1$d", this.abs);
+    }
+
+    @Override
+    public int compareTo(Data d) {
+        return Integer.compare(d.getAbs(), this.getAbs());
     }
 }
