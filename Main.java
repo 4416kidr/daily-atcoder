9e49712dec9736ed7bda4671e77aaec7e4124221
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2025-05-25 10:54:31 +0900

    mod: solveメソッドに切り分け

--- Main.java
+++ Main.java
@@ -7,38 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String series = sc.nextLine();
         sc.close();
-
-        List<Integer> mustSeries = new ArrayList<>();
-        List<Integer> maySeries = new ArrayList<>();
-        for (int i = 0; i < series.length(); i++) {
-            if (series.charAt(i) == 'x') {
-                continue;
-            }
-            if (series.charAt(i) == 'o') {
-                mustSeries.add(i);
-            } else if (series.charAt(i) == '?') {
-                maySeries.add(i);
-            }
-            continue;
-        }
-        System.out.println(String.format("must(%1$d), may(%2$d)", mustSeries.size(), maySeries.size()));
-
-        // int moreElm = 4 - mustSeries.size();
-        // int mayCount = ;
-        System.out.println(permutation(3) * maySeries.size());
-
-        // o012?345x67889
-        // 012/3, 012/4, 012/5
-        // 0/012, 1/012, 2/012
-        // 012, 021, 102, 120, 201, 210
-        // 3!*4*3 + 3!*4*3/2(重複分)
-        // 3*3!(4+2) = 18*6 = 108
-        // 0111, 0011, 0001
-        // 0011 -> 0011, 0101, 0110, 1001, 1010, 1100
-        for (int i = 1; i <= 4; i++) {
-            System.out.println(countMustAns(i));
-        }
-        System.out.println(countMayAns(0, 0));
+        solve(series);
     }
     private static int countMayAns(int mustC, int mayC) {
         return -1;
@@ -73,5 +42,39 @@ public class Main {
             res *= i;
         }
         return res;
+    }
+
+    private static void solve(String series) {
+        List<Integer> mustSeries = new ArrayList<>();
+        List<Integer> maySeries = new ArrayList<>();
+        for (int i = 0; i < series.length(); i++) {
+            if (series.charAt(i) == 'x') {
+                continue;
+            }
+            if (series.charAt(i) == 'o') {
+                mustSeries.add(i);
+            } else if (series.charAt(i) == '?') {
+                maySeries.add(i);
+            }
+            continue;
+        }
+        System.out.println(String.format("must(%1$d), may(%2$d)", mustSeries.size(), maySeries.size()));
+
+        // int moreElm = 4 - mustSeries.size();
+        // int mayCount = ;
+        System.out.println(permutation(3) * maySeries.size());
+
+        // o012?345x67889
+        // 012/3, 012/4, 012/5
+        // 0/012, 1/012, 2/012
+        // 012, 021, 102, 120, 201, 210
+        // 3!*4*3 + 3!*4*3/2(重複分)
+        // 3*3!(4+2) = 18*6 = 108
+        // 0111, 0011, 0001
+        // 0011 -> 0011, 0101, 0110, 1001, 1010, 1100
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(countMustAns(i));
+        }
+        System.out.println(countMayAns(0, 0));
     }
 }
