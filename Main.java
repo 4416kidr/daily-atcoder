1e560f1ada492c4b6442fe7a71e119471d7d3dde
TOFU <4416kidr@gmail.com>
2025-05-28 23:08:17 +0900

    add: 383B 手動でセットして、結果を出力できるようになった

--- Main.java
+++ Main.java
@@ -30,14 +30,17 @@ public class Main {
         debug("%s", CellType.CHAIR.getSymbol());
         debug("%s", CellType.FLOOR.getSymbol());
         debug("%s", office);
-        return -1;
+        office.setHumidifier(0, distance);
+        debug("\n");
+        office.setHumidifier(4, distance);
+        return office.countHumid();
     }
 }
 
-class Office {
+class Office implements Cloneable {
     private int width;
     private int height;
-    private List<CellType> map;
+    private List<CellType> flattenMap;
 
     public Office(List<String> map, int w, int h) {
         this.width = w;
@@ -45,7 +48,7 @@ class Office {
         String oneLine = map.stream().reduce("", (total, cur) -> total += cur);
         List<CellType> cells = new ArrayList<>(w*h);
         IntStream.range(0, oneLine.length()).forEach(i -> cells.add(CellType.fromChar(oneLine.charAt(i))));
-        this.map = cells;
+        this.flattenMap = cells;
         debug("Office: %s", cells);
     }
 
@@ -54,22 +57,69 @@ class Office {
     }
 
     public int size() {
-        // return this.getFlatten().length();
-        return -1;
+        return this.flattenMap.size();
+    }
+
+    public void setHumidifier(int pos, int pow) {
+        CellType toSetCell = this.flattenMap.get(pos);
+        if (!toSetCell.canSet()) {
+            debug("setHumidifier(%d): cant set at %d -> %s", pos, pos, toSetCell);
+            return;
+        }
+        debug("setHumidifier(%d): before all", pos);
+        debug("%s", this);
+        this.flattenMap.set(pos, CellType.HUMIDIFIER);
+        debug("setHumidifier(%d): after set", pos);
+        debug("%s", this);
+
+        // 5*5 -> 0: (1, 1) 3: (1, 3), 5: (1, 5) 17: (3, 2)
+        // diffuse
+        int ph = pos / this.width;
+        int pw = pos % this.height;
+        IntStream.range(0, this.size()).forEach(i -> {
+            CellType tCell = this.flattenMap.get(i);
+            if (!tCell.canSet() || tCell == CellType.HUMID) return;
+            int th = i / this.width;
+            int tw = i % this.width;
+            debug("(%d -> %d, %d), (%d -> %d, %d)", pos, ph, pw, i, th, tw);
+            int distance = Math.abs(ph - th) + Math.abs(pw - tw);
+            if (distance > pow) return;
+            this.flattenMap.set(i, CellType.HUMID);
+        });
+
+        debug("setHumidifier(%d): after humid", pos);
+        debug("%s", this);
+    }
+
+    public List<String> transform() {
+        List<String> list = new ArrayList<>(this.height);
+        IntStream.range(0, this.height).forEach(h -> {
+            List<String> tList = new ArrayList<>(this.width);
+            IntStream.range(0, this.width).forEach(w -> tList.add(String.valueOf(this.flattenMap.get(h*this.width + w).getSymbol())));
+            String str = tList.stream().reduce("", (acc, cur) -> acc += cur);
+            list.add(str);
+        });
+        return list;
+    }
+
+    public int countHumid() {
+        return (int)this.flattenMap.stream().filter(c -> c == CellType.HUMID || c == CellType.HUMIDIFIER).count();
+    }
+
+    @Override
+    public Office clone() {
+        try {
+            return (Office)super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("cant clone...", e.getCause());
+        }
     }
 
     @Override
     public String toString() {
-        List<String> list = new ArrayList<>(this.height);
-        IntStream.range(0, this.height).forEach(h -> {
-            List<String> tList = new ArrayList<>(this.width);
-            IntStream.range(0, this.width).forEach(w -> tList.add(String.valueOf(this.map.get(h*this.width + w).getSymbol())));
-            String str = tList.stream().reduce("", (acc, cur) -> acc += cur);
-            debug("%d: %s", h, str);
-            list.add(str);
-        });
+        List<String> list = this.transform();
         String res = list.stream().reduce("", (acc, cur) -> acc += "[DEBUG] " + cur + "\n");
-        return "--- Office ---\n" + res + "--- Office ---";
+        return "--- Office ---\n" + res + "[DEBUG] --- Office ---";
     }
 }
 
@@ -97,6 +147,11 @@ enum CellType {
 
     public char getSymbol() {
         return this.symbol;
+    }
+
+    public boolean canSet() {
+        boolean cantSet = this == CellType.CHAIR || this == CellType.HUMIDIFIER;
+        return !cantSet;
     }
 }
 
