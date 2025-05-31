0f109e51f1bcee73917857512c4f78d0fe076f4b
TOFU <4416kidr@gmail.com>
2025-05-31 18:02:06 +0900

    AC: 381C

--- Main.java
+++ Main.java
@@ -2,18 +2,34 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    // 381B
     Scanner sc = new Scanner(System.in);
-    final int N = sc.nextInt();
-    final int D = sc.nextInt();
-    sc.nextLine();
     String tStr = sc.nextLine();
     sc.close();
+    // System.out.println(tStr);
+    System.out.println(solve(tStr) ? "Yes" : "No");
+  }
 
-    int ind = -1;
-    for (int i = 0; i < D; i++) {
-        ind = tStr.lastIndexOf("@");
-        tStr = tStr.substring(0, ind) + "." + tStr.substring(ind+1, N);
+  private static boolean solve(String inpt) {
+    if (inpt.length() % 2 != 0) {
+        // System.out.println("ERROR: odd");
+        return false;
     }
-    System.out.println(tStr);
+    String containStr = "";
+    for (int i = 0; i < inpt.length() / 2; i++) {
+        // System.out.printf("%s != %s\n", inpt.charAt(2*i+1), inpt.charAt(2*i));
+        if (inpt.charAt(2*i + 1) != inpt.charAt(2*i)) {
+            // System.out.println("ERROR: series");
+            return false;
+        }
+        // System.out.printf("%s in %s \n", inpt.charAt(2*i), containStr);
+        if (containStr.contains(String.valueOf(inpt.charAt(2*i)))) {
+            // System.out.println("ERROR: contain");
+            return false;
+        }
+        containStr += inpt.charAt(2*i);
+        // System.out.println(containStr);
+    }
+    return true;
   }
 }
