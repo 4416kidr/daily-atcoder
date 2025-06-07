632e52bfb6db3009aa67623aefc598951a9b9851
TOFU <4416kidr@gmail.com>
2025-06-07 23:12:05 +0900

    AC: 388A

--- Main.java
+++ Main.java
@@ -1,23 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        // 389A
+        // 388A
         Scanner sc = new Scanner(System.in);
         final String inpt = sc.nextLine();
         sc.close();
-        // System.out.println(solve(inpt.charAt(0), inpt.charAt(2)));
-        System.out.println(solve2(inpt.charAt(0), inpt.charAt(2)));
-    }
-
-    private static int solve(char a, char b) {
-        return Integer.valueOf(String.valueOf(a)) * Integer.valueOf(String.valueOf(b));
-    }
-
-    private static int solve2(char a, char b) {
-        return (a - '0') * (b - '0');
+        System.out.println(inpt.charAt(0) + "UPC");
     }
 }
