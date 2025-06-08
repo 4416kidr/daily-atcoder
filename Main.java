876d7fb7e8b4ef665ea294239f5a9cbfe2813efd
TOFU <4416kidr@gmail.com>
2025-06-08 21:58:50 +0900

    AC: 395A

--- Main.java
+++ Main.java
@@ -1,92 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        // 377B
-        // 1848-2020
+        // 395A
         Scanner sc = new Scanner(System.in);
-        final List<String> list = new ArrayList<>();
-        IntStream.range(0, 8).forEach(i -> list.add(sc.nextLine()));
+        final int N = sc.nextInt();
+        final List<Integer> list = new ArrayList<>();
+        IntStream.range(0, N).forEach(i -> list.add(sc.nextInt()));
         sc.close();
-        Board board = new Board(list);
-        System.out.println(solve(board));
-    }
-
-    private static int solve(Board board) {
-        Board paintedBoard = board.paintAll();
-        return paintedBoard.countCanPaint();
-    }
-}
-
-class Board {
-    private final String board;
-    private final int width;
-    private final int height;
-
-    public Board(List<String> list) {
-        this.board = list.stream().reduce("", (acc, cur) -> acc + cur);
-        this.width = 8;
-        this.height = 8;
-    }
-
-    public Board(String str) {
-        this.board = str;
-        this.width = 8;
-        this.height = 8;
-        if (str.length() != this.width * this.height) {
-            throw new RuntimeException("not expected size");
-        }
-    }
-
-    public Board paintAll() {
-        Board finalBoard = IntStream.range(0, this.height)
-            .boxed()
-            .reduce(
-                this, 
-                (prevLine, h) -> IntStream.range(0, this.width)
-                    .boxed()
-                    .reduce(prevLine, (prev, w) -> isPiece(translatePoint(w, h)) ? prev.paintFromPoint(w, h) : prev, (a, b) -> b),
-                (a, b) -> b
-            );
-        return finalBoard;
-    }
-
-    private Board paintFromPoint(int fw, int fh) {
-        // same width (vertical)
-        // Javaの型推論の都合により、reduceの第1引数とStream<T>の型が違う場合、2引数版のreduceは使えないので、3引数版reduceを使う必要がある
-        // 第3引数combinerは並列処理に使う引数で、今回は使われないが安全で副作用のない実装をダミーとして置くのが通例
-        // (a, b) -> b がよく使われる
-        final Board verticalBoard = IntStream.range(0, this.height).boxed().reduce(this, (prev, i) -> i == fh ? prev : prev.paintPoint(translatePoint(fw, i)), (a, b) -> b);
-        return IntStream.range(0, this.width).boxed().reduce(verticalBoard, (prev, i) -> i == fw ? prev : prev.paintPoint(translatePoint(i, fh)), (a, b) -> b);
-    }
-
-    private Board paintPoint(int flatPoint) {
-        if (!canPaint(flatPoint)) {
-            return this;
-        }
-        return new Board(this.board.substring(0, flatPoint) + "x" + this.board.substring(flatPoint+1));
-    }
-
-    private boolean canPaint(int flatPoint) {
-        final char res = board.charAt(flatPoint);
-        return res == '.';
-    }
-
-    private boolean isPiece(int flatPoint) {
-        final char res = board.charAt(flatPoint);
-        return res == '#';
-    }
-
-    private int translatePoint(int w, int h) {
-        return this.width * h + w;
-    }
-
-    public int countCanPaint() {
-        return IntStream.range(0, this.width * this.height).reduce(0, (count, i) -> canPaint(i) ? ++count : count);
-    }
-
-    @Override
-    public String toString() {
-        return IntStream.range(0, this.height).mapToObj(i -> board.substring(this.width*i, this.width*(i+1) - 1)).reduce("", (acc, cur) -> acc + cur + "\n");
+        boolean flag = IntStream.range(1, N).allMatch(i -> list.get(i-1) < list.get(i));
+        System.out.println(flag ? "Yes" : "No");
     }
 }
