2ea12e2fdb19a4acece9314bb6e5cccfc3dcb069
TOFU <4416kidr@gmail.com>
2025-06-15 23:12:27 +0900

    AC: 386A

--- Main.java
+++ Main.java
@@ -1,21 +1,64 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        // 392A
-        // 2330-2340
+        // 386A
+        // 2220-2310
         final Scanner sc = new Scanner(System.in);
-        final int A = sc.nextInt();
-        final int B = sc.nextInt();
-        final int C = sc.nextInt();
+        System.out.println(solveByList(sc) ? "Yes" : "No");
         sc.close();
-        System.out.println(solve(A, B, C) ? "Yes" : "No");
     }
 
-    private static boolean solve(int a, int b, int c) {
-        final boolean ab = a*b == c;
-        final boolean bc = b*c == a;
-        final boolean ca = c*a == b;
-        return ab || bc || ca;
+    public static boolean solve(Scanner sc) {
+        final Map<Integer, Integer> map = new HashMap<>();
+        IntStream.range(0, 4).forEach(i -> {
+            final int inpt = sc.nextInt();
+            if (map.containsKey(inpt)) {
+                map.put(inpt, map.get(inpt) + 1);
+            } else {
+                map.put(inpt, 1);
+            }
+        });
+        return map.keySet().size() == 2;
+    }
+
+    public static boolean solveByList(Scanner sc) {
+        final List<Integer> list = new ArrayList<>();
+        IntStream.range(0, 4).forEach(i -> {
+            final int inpt = sc.nextInt();
+            if (list.contains(inpt)) {
+                return;
+            }
+            list.add(inpt);
+        });
+        return list.size() == 2;
+    }
+
+    public static boolean solveBySimple(Scanner sc) {
+        int a = -1;
+        int b = -1;
+        for (int i = 0; i < 4; i++) {
+            final int inpt = sc.nextInt();
+            if (inpt != a && inpt != b) {
+                return false;
+            }
+            if (a == inpt || b == inpt) {
+                continue;
+            }
+            if (a == -1) {
+                a = inpt;
+                continue;
+            }
+            if (b == -1) {
+                b = inpt;
+                continue;
+            } 
+        }
+        return a != -1 && b != -1;
     }
 }
