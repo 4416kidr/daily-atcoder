525fdb644d8975b2b3b239af8a39f445e6a510cf
T00FUU <52730034+4416kidr@users.noreply.github.com>
2025-06-17 00:29:38 +0900

    WA: 385A

--- Main.java
+++ Main.java
@@ -1,64 +1,23 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        // 386A
-        // 2220-2310
+        // 385A
+        // 0857-0906
         final Scanner sc = new Scanner(System.in);
-        System.out.println(solveByList(sc) ? "Yes" : "No");
+        final List<Integer> list = List.of(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        System.out.println(solve(list) ? "Yes" : "No");
         sc.close();
     }
 
-    public static boolean solve(Scanner sc) {
-        final Map<Integer, Integer> map = new HashMap<>();
-        IntStream.range(0, 4).forEach(i -> {
-            final int inpt = sc.nextInt();
-            if (map.containsKey(inpt)) {
-                map.put(inpt, map.get(inpt) + 1);
-            } else {
-                map.put(inpt, 1);
-            }
-        });
-        return map.keySet().size() == 2;
-    }
-
-    public static boolean solveByList(Scanner sc) {
-        final List<Integer> list = new ArrayList<>();
-        IntStream.range(0, 4).forEach(i -> {
-            final int inpt = sc.nextInt();
-            if (list.contains(inpt)) {
-                return;
-            }
-            list.add(inpt);
-        });
-        return list.size() == 2;
-    }
-
-    public static boolean solveBySimple(Scanner sc) {
-        int a = -1;
-        int b = -1;
-        for (int i = 0; i < 4; i++) {
-            final int inpt = sc.nextInt();
-            if (inpt != a && inpt != b) {
-                return false;
-            }
-            if (a == inpt || b == inpt) {
-                continue;
-            }
-            if (a == -1) {
-                a = inpt;
-                continue;
-            }
-            if (b == -1) {
-                b = inpt;
-                continue;
-            } 
-        }
-        return a != -1 && b != -1;
+    public static boolean solve(List<Integer> list) {
+        final boolean three = list.stream().allMatch(v -> list.get(0) == v);
+        if (three) return true;
+        final List<Boolean> flags = IntStream.range(0, 3)
+                                        .mapToObj(i -> list.get(i) == (list.get((i+1) % 3) + list.get((i+2) % 3)))
+                                        .toList();
+        return flags.contains(true);
     }
 }
