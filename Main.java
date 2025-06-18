037851edb4c8f0b4be3284031ffecd297b9ca759
TOFU <4416kidr@gmail.com>
2025-06-18 23:13:11 +0900

    AC: 385A

--- Main.java
+++ Main.java
@@ -11,14 +11,20 @@ public class Main {
         List<Integer> list = new ArrayList<>();
         IntStream.range(0, 3).forEach(i -> list.add(sc.nextInt()));
         sc.close();
-        System.out.println(solve(list.get(0), list.get(1), list.get(2)) ? "Yes" : "No");
+        System.out.println(solve(list) ? "Yes" : "No");
     }
 
-    public static boolean solve(int a, int b, int c) {
-        final boolean all = a == b && a == c;
-        final boolean ab = a + b == c;
-        final boolean bc = b + c == a;
-        final boolean ca = c + a == b;
-        return all || ab || bc || ca;
+    public static boolean solve(List<Integer> list) {
+        if (list.size() != 3) {
+            return false;
+        }
+        // Integerに対して、equalsじゃなくて、==を使っていたのがWAの原因
+        final boolean three = list.get(0).equals(list.get(1))  && list.get(0).equals(list.get(2));
+        if (three) {
+            return true;
+        };
+        list.sort((a, b) -> a-b);
+        final boolean two = list.get(0) + list.get(1) == list.get(2);
+        return two;
     }
 }
