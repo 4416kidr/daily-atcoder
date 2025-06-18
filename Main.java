71c5be1d10d95714c63363022b0e32339a988b53
TOFU <4416kidr@gmail.com>
2025-06-18 22:52:03 +0900

    AC: 385A

--- Main.java
+++ Main.java
@@ -11,23 +11,14 @@ public class Main {
         List<Integer> list = new ArrayList<>();
         IntStream.range(0, 3).forEach(i -> list.add(sc.nextInt()));
         sc.close();
-        System.out.println(solve(list) ? "Yes" : "No");
+        System.out.println(solve(list.get(0), list.get(1), list.get(2)) ? "Yes" : "No");
     }
 
-    public static boolean solve(List<Integer> list) {
-        // System.out.println(list);
-        if (list.size() != 3) {
-            return false;
-        }
-        final boolean three = list.get(0) == list.get(1) && list.get(0) == list.get(2);
-        // System.out.println(three);
-        if (three) {
-            return true;
-        };
-        list.sort((a, b) -> a-b);
-        // System.out.println(list);
-        final boolean two = list.get(0) + list.get(1) == list.get(2);
-        // System.out.println(two);
-        return two;
+    public static boolean solve(int a, int b, int c) {
+        final boolean all = a == b && a == c;
+        final boolean ab = a + b == c;
+        final boolean bc = b + c == a;
+        final boolean ca = c + a == b;
+        return all || ab || bc || ca;
     }
 }
